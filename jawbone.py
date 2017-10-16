f7bfd50ff358b3b5a5bf4ff61640c5e498839298
Robert Friel <rfriel@gmail.com>
2017-10-16 11:51:07 -0700

    fixed cookies error; made executable script jawbone.py

--- jawbone.py
+++ jawbone.py
@@ -1,13 +1,32 @@
+#!/usr/bin/env python
+
+import sys
 import requests
 
 def cookies():
-    with open('cookies.txt') as f:
-        jbsession_val = f.read().rstrip()
-    return {'jbsession': jbsession_val}
+    try:
+        with open('cookies.txt') as f:
+            jbsession_val = f.read().rstrip()
+        return {'jbsession': jbsession_val}
+    except IOError:
+        print 'File cookies.txt not found.'
 
 def get_data(year):
-    url = 'https://jawbone.com/user/settings/download_up_data?year=' + str(year)
+    url = 'https://jawbone.com/user/settings/download_up_data?year=' + year
     response = requests.get(url, cookies=cookies())
 
-    with open(str(year) + '.csv','w') as f:
+    csv_file = year + '.csv'
+    with open(csv_file, 'w') as f:
         f.write(response.content)
+
+def print_help():
+    print 'Usage: jawbone.py year' + '\n'
+    print 'Retrieves data for the given year from the jawbone website.'
+
+if __name__ == '__main__':
+    if len(sys.argv) <= 1:
+        print_help()
+        sys.exit()
+
+    year = sys.argv[1]
+    get_data(year)
